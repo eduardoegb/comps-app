973be17f75d23a0360c166ef8b9183e36e7cb073
Eduardo Gonzalez Briceno <eduardoegb@gmail.com>
2023-01-24 17:55:43 -0700

    Added routing and navigation

--- src/app/elements/elements.module.ts
+++ src/app/elements/elements.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { ElementsRoutingModule } from './elements-routing.module';
+import { ElementsHomeComponent } from './elements-home/elements-home.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    ElementsHomeComponent
+  ],
+  exports: [],
   imports: [
     CommonModule,
     ElementsRoutingModule
